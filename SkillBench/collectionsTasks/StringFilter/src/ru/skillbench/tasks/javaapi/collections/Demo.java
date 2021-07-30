e2c1ab5a7762d36bbf528c655e988bb376c4586b
zigal0 <efimov.se@phystech.edu>
2021-07-30 18:57:26 +0300

    Collections Tasks completed

--- SkillBench/collectionsTasks/StringFilter/src/ru/skillbench/tasks/javaapi/collections/Demo.java
+++ SkillBench/collectionsTasks/StringFilter/src/ru/skillbench/tasks/javaapi/collections/Demo.java
@@ -1,7 +1,83 @@
 package ru.skillbench.tasks.javaapi.collections;
 
+import java.util.*;
+
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String separator = "*********************************************";
+        StringFilter list = new StringFilterImpl();
+        list.add("Lol");
+        list.add(null);
+        list.add("Kek");
+        list.add("CheBureck");
+        list.add("Lol");
+        list.add("NiggeR");
+        list.remove("nIgger");
+        list.add("Chicken");
+        list.add("Char");
+        list.add("91");
+        list.add("distribute");
+        list.add("(123)456-7890");
+        list.add("-7.55");
+        list.add("1000");
+        list.add("1 000");
+        list.add("protection");
+        list.add("copyright");
+        list.add("charge");
+        list.add("original");
+        list.add("guarantee");
+        list.add("program");
+        list.add("danger");
+        list.add("verbatim");
+        list.add("redistributors");
+        list.add("recipients");
+        list.add("restricted");
+        list.add("outside");
+        list.add("instead");
+        list.add("restest");
+        list.add("chcher");
+
+
+        Collection<String> full = list.getCollection();
+        for (String word : full) {
+            System.out.println(word);
+        }
+        System.out.println(separator);
+
+        Iterator<String> collection = list.getStringsContaining("ol");
+        if (!collection.hasNext()) {
+            System.out.println("Empty");
+        }
+        while (collection.hasNext()) {
+            System.out.println(collection.next());
+        }
+        System.out.println(separator);
+
+        collection = list.getStringsStartingWith("Ch");
+        if (!collection.hasNext()) {
+            System.out.println("Empty");
+        }
+        while (collection.hasNext()) {
+            System.out.println(collection.next());
+        }
+        System.out.println(separator);
+
+        collection = list.getStringsByNumberFormat("####");
+        if (!collection.hasNext()) {
+            System.out.println("Empty");
+        }
+        while (collection.hasNext()) {
+            System.out.println(collection.next());
+        }
+        System.out.println(separator);
+
+        collection = list.getStringsByPattern("*ch*ch*");
+        if (!collection.hasNext()) {
+            System.out.println("Empty");
+        }
+        while (collection.hasNext()) {
+            System.out.println(collection.next());
+        }
+        System.out.println(separator);
     }
 }
--- SkillBench/collectionsTasks/StringFilter/src/ru/skillbench/tasks/javaapi/collections/StringFilterImpl.java
+++ SkillBench/collectionsTasks/StringFilter/src/ru/skillbench/tasks/javaapi/collections/StringFilterImpl.java
@@ -1,46 +1,149 @@
 package ru.skillbench.tasks.javaapi.collections;
 
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 public class StringFilterImpl implements StringFilter {
+    private final Set<String> set = new HashSet<>();
+
     @Override
     public void add(String s) {
-
+        if (s == null) {
+            set.add(null);
+        } else {
+            set.add(s.toLowerCase());
+        }
     }
 
     @Override
     public boolean remove(String s) {
-        return false;
+        if (s == null) {
+            return set.remove(null);
+        }
+        return set.remove(s.toLowerCase());
     }
 
     @Override
     public void removeAll() {
-
+        set.clear();
     }
 
     @Override
     public Collection<String> getCollection() {
-        return null;
+        return set;
     }
 
     @Override
-    public Iterator<String> getStringsContaining(String chars) {
-        return null;
+    public Iterator<String> getStringsContaining(final String chars) {
+        if (chars == null || chars.equals("")) {
+            return set.iterator();
+        }
+        Filter filter = new Filter() {
+            @Override
+            public boolean check(String forCheck) {
+                return forCheck.contains(chars);
+            }
+        };
+        return findWords(filter);
     }
 
     @Override
-    public Iterator<String> getStringsStartingWith(String begin) {
-        return null;
+    public Iterator<String> getStringsStartingWith(final String begin) {
+        if (begin == null || begin.equals("")) {
+            return set.iterator();
+        }
+        Filter filter = new Filter() {
+            @Override
+            public boolean check(String forCheck) {
+                String beginLower = begin.toLowerCase();
+                return forCheck.startsWith(beginLower);
+            }
+        };
+        return findWords(filter);
     }
 
     @Override
-    public Iterator<String> getStringsByNumberFormat(String format) {
-        return null;
+    public Iterator<String> getStringsByNumberFormat(final String format) {
+        if (format == null || format.equals("")) {
+            return set.iterator();
+        }
+        Filter filter = new Filter() {
+            @Override
+            public boolean check(String forCheck) {
+                char[] subForCheck = forCheck.toCharArray();
+                char[] subFormat = format.toCharArray();
+                if (subForCheck.length != subFormat.length) {
+                    return false;
+                }
+                for (int i = 0; i < subForCheck.length; i++) {
+                    if (subForCheck[i] != subFormat[i]) {
+                        if (subFormat[i] == '#' && Character.isDigit(subForCheck[i])) {
+                            continue;
+                        }
+                        return false;
+                    }
+                }
+                return true;
+            }
+        };
+        return findWords(filter);
     }
 
     @Override
-    public Iterator<String> getStringsByPattern(String pattern) {
-        return null;
+    public Iterator<String> getStringsByPattern(final String pattern) {
+        if (pattern == null || pattern.equals("")) {
+            return set.iterator();
+        }
+        Filter filter = new Filter() {
+            @Override
+            public boolean check(String forCheck) {
+                String own = forCheck;
+                boolean start = false;
+                boolean finish = false;
+                if (pattern.charAt(0) != '*') {
+                    start = true;
+                }
+                if (pattern.charAt(pattern.length() - 1) != '*') {
+                    finish = true;
+                }
+                String[] parts = pattern.split("\\*");
+                for (int i = 0; i < parts.length; i++) {
+                    if (i == 0 && start) {
+                        if (!own.startsWith(parts[i])) {
+                            return false;
+                        }
+                        own = own.substring(parts[i].length());
+                    } else if (i == parts.length - 1 && finish) {
+                        if (!own.endsWith(parts[i])) {
+                            return false;
+                        }
+                    } else {
+                        if (!own.contains(parts[i])) {
+                            return false;
+                        }
+                        own = own.substring(own.indexOf(parts[i]) + parts[i].length());
+                    }
+                }
+                return true;
+            }
+        };
+        return findWords(filter);
+    }
+
+    private Iterator<String> findWords(Filter filter) {
+        List<String> result = new ArrayList<>();
+        for (String word : set) {
+            if (word == null) {
+                continue;
+            }
+            if (filter.check(word)) {
+                result.add(word);
+            }
+        }
+        return result.iterator();
+
+    }
+
+    private interface Filter {
+        boolean check(String forCheck);
     }
 }
