9c35c7c5a8594bd2a2ea78b22f61e201e20d65a7
zigal0 <efimov.se@phystech.edu>
2021-08-10 19:05:12 +0300

    Some fixes

--- SkillBench/javaAPITasks/wordFinder/src/ru/skillbench/tasks/javaapi/io/Demo.java
+++ SkillBench/javaAPITasks/wordFinder/src/ru/skillbench/tasks/javaapi/io/Demo.java
@@ -2,6 +2,7 @@ package ru.skillbench.tasks.javaapi.io;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -13,7 +14,11 @@ public class Demo {
         String example = "example.txt";
         WordFinderImpl wordFinder = new WordFinderImpl();
         wordFinder.setFilePath(example);
-        Stream<String> result = wordFinder.findWordsStartWith("Wh");
         System.out.println(wordFinder.getText());
+        System.out.println("findWordsStartWith: ");
+        Stream<String> result = wordFinder.findWordsStartWith("Wh");
+        System.out.println(Arrays.toString(result.toArray()));
+        System.out.println("Method writeWords: ");
+        wordFinder.writeWords(System.out);
     }
 }
